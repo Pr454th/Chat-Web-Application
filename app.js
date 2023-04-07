eebbaf26d90513b35550714b8cfb36751ee5da15
pr454th <prasathkarthiban1999@gmail.com>
2023-04-07 12:06:30 +0530

    deploy vercel

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/dist"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
