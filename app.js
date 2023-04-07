e08d2c57da79d2747be61fada3e3d0e9af16a363
pr454th <prasathkarthiban1999@gmail.com>
2023-04-07 11:23:45 +0530

    deploy vercel

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ app.use("/api/chat", chatRoutes(io));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/dist"));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "frontend", "dist", "dist/index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
