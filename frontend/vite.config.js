b91d0b38588a57327f79ff76b63e23ecf99cc6aa
pr454th <prasathkarthiban1999@gmail.com>
2023-04-08 10:05:29 +0530

    deploy vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "http://127.0.0.1:3001",
+        ws: true,
         secure: false,
         changeOrigin: true,
         configure: (proxy, _options) => {
